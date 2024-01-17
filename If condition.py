e7666f9884fd8b70e6cd73d71a6020861c0ea17f
Jaeycode <99529655+Jaey12@users.noreply.github.com>
2024-01-17 12:19:54 +0530

    Add files via upload

---
+++ If condition.py
@@ -0,0 +1,21 @@
+
+
+num=1000
+num1=100
+
+if num==num1:
+    print("Smallest number is:", num)
+elif num<num1:
+    print("num is a smallest number it is:", num)
+else:
+    print("No condition satisfied")
+
+number = 7
+if number + 1 == number % 2:
+    print("Even")
+else:
+    print("Odd")
+
+
+
+
